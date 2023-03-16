1981a8b2b60a34f4d6957312c61b1fe074d06286
Nell <eemandersson@outlook.com>
2023-03-16 07:26:22 +0100

    wrote some code

--- game.js
+++ game.js
@@ -30,43 +30,43 @@ class Game {
 		const player2 = this.player2;
 
 		if (player1.height > player2.height) {
-			result.tallest = `${player1.name} (${player1.height}) is taller than ${player2.name} (${player2.height})`;
+			result.tallest = `${player1.name} (${player1.height} cm) is taller than ${player2.name} (${player2.height} cm).`;
 		} else if (player1.height === player2.height) {
-			result.tallest = `${player2.name} (${player2.height}) is equal to ${player1.name} (${player1.height})`;
+			result.tallest = `${player2.name} (${player2.height} cm) is equal to ${player1.name} (${player1.height} cm).`;
 		} else {
-			result.tallest = `${player2.name} (${player2.height}) is taller than ${player1.name} (${player1.height})`;
+			result.tallest = `${player2.name} (${player2.height} cm) is taller than ${player1.name} (${player1.height} cm).`;
 		}
 
 		if (player1.mass > player2.mass) {
-			result.heaviest = `${player1.name} (${player1.mass}) is heavier than ${player2.name} (${player2.mass})`;
+			result.heaviest = `${player1.name} (${player1.mass} kg) is heavier than ${player2.name} (${player2.mass} kg)`;
 		} else if (player1.mass === player2.mass) {
-			result.heaviest = `${player2.name} (${player2.mass}) is equal to ${player1.name} (${player1.mass})`;
+			result.heaviest = `${player2.name} weight (${player2.mass} kg) is equal to ${player1.name} weight (${player1.mass} kg).`;
 		} else {
-			result.heaviest = `${player2.name} (${player2.mass}) is heavier than ${player1.name} (${player1.mass})`;
+			result.heaviest = `${player2.name} (${player2.mass} kg) is heavier than ${player1.name} (${player1.mass} kg).`;
 		}
 
 		if (player1.gender === player2.gender) {
-			result.gender = `${player1.name} (${player1.gender}) is the same gender as ${player2.name} (${player2.gender})`;
+			result.gender = `${player1.name} (${player1.gender}) is the same gender as ${player2.name} (${player2.gender}).`;
 		} else {
-			result.gender = `${player2.name} (${player2.gender}) is not the same gender as ${player1.name} (${player1.gender})`;
+			result.gender = `${player2.name} (${player2.gender}) is not the same gender as ${player1.name} (${player1.gender}).`;
 		}
 
 		if (player1.skinColor === player2.skinColor) {
-			result.skinColor = `${player1.name} (${player1.skinColor}) has the same skin color as ${player2.name} (${player2.skinColor})`;
+			result.skinColor = `${player1.name} (${player1.skinColor}) has the same skin color as ${player2.name} (${player2.skinColor}).`;
 		} else {
-			result.skinColor = `${player2.name} (${player2.skinColor}) dont have the same skin color as ${player1.name} (${player1.skinColor})`;
+			result.skinColor = `${player2.name} (${player2.skinColor}) don't have the same skin color as ${player1.name} (${player1.skinColor}).`;
 		}
 
 		if (player1.eyeColor === player2.eyeColor) {
-			result.eyeColor = `${player1.name} (${player1.eyeColor}) has the same eye color as ${player2.name} (${player2.eyeColor})`;
+			result.eyeColor = `${player1.name} (${player1.eyeColor}) has the same eye color as ${player2.name} (${player2.eyeColor}).`;
 		} else {
-			result.eyeColor = `${player2.name} (${player2.eyeColor}) dont have the same eye color as ${player1.name} (${player1.eyeColor})`;
+			result.eyeColor = `${player2.name} (${player2.eyeColor}) don't have the same eye color as ${player1.name} (${player1.eyeColor}).`;
 		}
 
 		if (player1.hairColor === player2.hairColor) {
-			result.hairColor = `${player1.name} (${player1.hairColor}) has the same hair color as ${player2.name} (${player2.hairColor})`;
+			result.hairColor = `${player1.name} (${player1.hairColor}) has the same hair color as ${player2.name} (${player2.hairColor}).`;
 		} else {
-			result.hairColor = `${player2.name} (${player2.hairColor}) dont have the same hair color as ${player1.name} (${player1.hairColor})`;
+			result.hairColor = `${player2.name} (${player2.hairColor}) don't have the same hair color as ${player1.name} (${player1.hairColor}).`;
 		}
 
 		return result;
